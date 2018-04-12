436e6855c20ddb06af9b8109b7b2fed664c0fa32
adamgayuh29 <adam.gayuh@gmail.com>
2018-04-12 16:30:53 +0700

    first commit

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layout')
+@section('content')
+	<table class="table table-bordered">
+		<tr>
+			<font="bold">
+			<td>No</td>
+			<td>Category Name</td>
+			<td>Action</td>
+		</tr>
+	</table>
+@endsection
